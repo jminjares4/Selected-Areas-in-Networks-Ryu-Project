0c19edd2fb3e884b2dc1fe2e9ddaabac9c42d2f9
jminjares4 <minjares.jesus.ths@gmail.com>
2022-05-03 10:37:56 -0600

    Added layer 3 topology example

--- mininet-topo/layer3.sh
+++ mininet-topo/layer3.sh
@@ -1,1 +1,1 @@
-sudo mn --controller=remote,ip=127.0.0.1 --mac --switch=ovsk,protocols=OpenFlow13 --topo=single,4
+sudo mn --controller=remote,ip=127.0.0.1 --mac -i 10.1.1.0/24 --switch=ovsk,protocols=OpenFlow13 --topo=tree,depth=3,fanout=4
